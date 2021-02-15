d2cb031d6b24ffe13f47c611ec66a847a0098f0f
lightyen <lightyen0123@gmail.com>
2021-02-15 15:32:53 +0800

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,18 +12,5 @@ module.exports = {
 			backgroundColor: ["group-focus"],
 		},
 	},
-	plugins: [
-		require("@tailwindcss/typography"),
-		function ({ addUtilities, addComponents, e, prefix, config }) {
-			// Add your custom styles here
-			addUtilities({
-				".test-10": {
-					color: "#100",
-				},
-				".test-50": {
-					color: "#400",
-				},
-			})
-		},
-	],
+	plugins: [],
 }
