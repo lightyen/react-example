fa5b8e45689a5701c0f8e51656f29c2dd4f8736d
lightyen <lightyen0123@gmail.com>
2021-04-26 14:29:22 +0800

    update

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,8 +1,31 @@
+import { animated, Globals, useSpring } from "@react-spring/web"
 import "dayjs/locale/zh-tw"
+import { useState } from "react"
 import { FormattedMessage } from "react-intl"
 import "twin.macro"
 import ReactLogo from "~/assets/logo.svg"
 import Page from "~/components/Page"
+
+Globals.assign({
+	skipAnimation: false,
+})
+
+function A() {
+	const [open, toggle] = useState(false)
+	const width = 256
+	const props = useSpring({ width: open ? width : 0 })
+
+	return (
+		<div tw="">
+			<div tw="w-64 relative" onClick={() => toggle(!open)}>
+				<animated.div tw="absolute bg-blue-800 top-0 h-full" style={props} />
+				<animated.div tw="relative bg-opacity-0 text-white text-center select-none">
+					{props.width.to(x => x.toFixed(0))}
+				</animated.div>
+			</div>
+		</div>
+	)
+}
 
 export default function Home() {
 	return (
@@ -14,6 +37,7 @@ export default function Home() {
 			"
 			/>
 			<FormattedMessage id="test" values={{ name: "React" }} />
+			<A />
 		</Page>
 	)
 }
