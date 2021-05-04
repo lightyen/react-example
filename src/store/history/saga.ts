4fd584a1b1351da5d64ccf0a155de0374160e2f9
lightyen <lightyen0123@gmail.com>
2021-05-04 08:56:39 +0800

    update

--- src/store/history/saga.ts
+++ src/store/history/saga.ts
@@ -1,8 +1,7 @@
-import { delay, fork, put } from "redux-saga/effects"
-import { push } from "./action"
+import { delay, fork } from "redux-saga/effects"
 export default function* history() {
 	yield fork(function* () {
 		yield delay(5000)
-		yield put(push("/transition"))
+		// yield put(push("/transition"))
 	})
 }
